7e502c6668ccfa01a0209af4dafb437d90037bc9
Thomsen Cummings <thomsencummings@gmail.com>
2021-03-27 17:14:40 -0400

    Add webpack bundle analyzer with analyzer scripts

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -1,10 +1,29 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 const { PORT } = require("../config");
 
 require("dotenv").config();
+
+// Add in the bundle analyzer if we passed in the proper env variable to do so
+let plugins = [
+  new HtmlWebPackPlugin({
+    template: "./src/index_template.html",
+    filename: "./index.html",
+  }),
+  new webpack.DefinePlugin({
+    "process.env": {
+      PORT: JSON.stringify(PORT),
+      PIXABAY_API_KEY: JSON.stringify(process.env.PIXABAY_API_KEY),
+    },
+  }),
+];
+if (process.env.ANALYZE_WEBPACK_BUNDLE) {
+  plugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = {
   entry: "./src/index.js",
@@ -36,16 +55,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index_template.html",
-      filename: "./index.html",
-    }),
-    new webpack.DefinePlugin({
-      "process.env": {
-        PORT: JSON.stringify(PORT),
-        PIXABAY_API_KEY: JSON.stringify(process.env.PIXABAY_API_KEY),
-      },
-    }),
-  ],
+  plugins,
 };
