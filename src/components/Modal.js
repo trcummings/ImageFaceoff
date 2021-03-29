742e0cd6ae018a78670acd7fab3444e57685c3a6
Thomsen Cummings <thomsencummings@gmail.com>
2021-03-28 21:43:42 -0400

    update Modal forwardRef export default

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,48 +13,47 @@ import React, {
 } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = (
-  { children, closeOnEsc = true, isOpenByDefault = false },
-  modalRef
-) => {
-  const [isOpen, setOpen] = useState(isOpenByDefault);
+const Modal = forwardRef(
+  ({ children, closeOnEsc = true, isOpenByDefault = false }, modalRef) => {
+    const [isOpen, setOpen] = useState(isOpenByDefault);
 
-  // This allows us to expose methods on the parent ref e.g. `modalRef.current.open()`
-  useImperativeHandle(modalRef, () => ({
-    open: () => setOpen(true),
-    close: () => setOpen(false),
-  }));
+    // This allows us to expose methods on the parent ref e.g. `modalRef.current.open()`
+    useImperativeHandle(modalRef, () => ({
+      open: () => setOpen(true),
+      close: () => setOpen(false),
+    }));
 
-  // Only use this effect hook if the parent component requests it
-  if (closeOnEsc) {
-    // keyCode 27 is the Escape key
-    const onKeydown = useCallback((event) => {
-      if (event.keyCode === 27) setOpen(false);
-    });
+    // Only use this effect hook if the parent component requests it
+    if (closeOnEsc) {
+      // keyCode 27 is the Escape key
+      const onKeydown = useCallback((event) => {
+        if (event.keyCode === 27) setOpen(false);
+      });
 
-    // Pass in onKeydown and isOpen as dependencies so it adds and removes the listeners when
-    // the modal opens and closes
-    useEffect(() => {
-      if (isOpen) document.addEventListener("keydown", onKeydown, false);
-      // Cleanup function on unmount
-      return () => document.removeEventListener("keydown", onKeydown, false);
-    }, [onKeydown, isOpen]);
+      // Pass in onKeydown and isOpen as dependencies so it adds and removes the listeners when
+      // the modal opens and closes
+      useEffect(() => {
+        if (isOpen) document.addEventListener("keydown", onKeydown, false);
+        // Cleanup function on unmount
+        return () => document.removeEventListener("keydown", onKeydown, false);
+      }, [onKeydown, isOpen]);
+    }
+
+    // Create the portal on the document body on open
+    return createPortal(
+      isOpen ? (
+        <div className="modal modal-open">
+          <div className="modal__background" />
+          {children}
+        </div>
+      ) : (
+        <div className="modal">
+          <div className="modal__background" />
+        </div>
+      ),
+      document.body
+    );
   }
+);
 
-  // Create the portal on the document body on open
-  return createPortal(
-    isOpen ? (
-      <div className="modal modal-open">
-        <div className="modal__background" />
-        {children}
-      </div>
-    ) : (
-      <div className="modal">
-        <div className="modal__background" />
-      </div>
-    ),
-    document.body
-  );
-};
-
-export default forwardRef(Modal);
+export default Modal;
